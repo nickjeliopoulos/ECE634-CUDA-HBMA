2f5fa4a2e3314f16964c49ab7ccd0738cb931f71
Nick John Eliopoulos <nickjeliopoulosdev@gmail.com>
2025-03-23 21:16:13 -0400

    Added .CSV and Markdown latency dumping. Will add PSNR dump

--- benchmark.py
+++ benchmark.py
@@ -10,6 +10,7 @@ from hbma.torch_naive_hbma import HBMA_Naive
 from hbma.torch_fused_cuda_hbma import HBMA_CUDA_Fused
 from hbma.utils import loss_PSNR
 import os
+import pandas
 
 ### Benchmark Function
 ### Adapted from https://github.com/pjjajal/nutils/blob/main/nutils/benchmark.py
@@ -34,6 +35,7 @@ def main(args: argparse.Namespace) -> None:
 	W_CROP = 256
 	H = 224
 	W = 224
+	BENCHMARK_TRIAL_COUNT = 2
 
 	### Initialize transforms
 	### Point is to standardize the input size
@@ -51,8 +53,8 @@ def main(args: argparse.Namespace) -> None:
 	anchor_tensor = transform(anchor_image).unsqueeze(0).contiguous()
 	target_tensor = transform(target_image).unsqueeze(0).contiguous()
 
-	print(f"Anchor Tensor Shape: {anchor_tensor.shape}")
-	print(f"Target Tensor Shape: {target_tensor.shape}")
+	# print(f"Anchor Tensor Shape: {anchor_tensor.shape}")
+	# print(f"Target Tensor Shape: {target_tensor.shape}")
 
 	### Display images
 	### Torch CPU HBMA (Baseline)
@@ -85,6 +87,56 @@ def main(args: argparse.Namespace) -> None:
 	motion_vectors = motion_vectors / torch.max(motion_vectors)
 	torchvision.utils.save_image( motion_vectors.squeeze(0), os.path.join(args.output_dir, "fused_cuda_hbma_motion.png"))
 	torchvision.utils.save_image( predicted_frame.squeeze(0), os.path.join(args.output_dir, "fused_cuda_hbma_predicted.png"))
+
+	###
+	### Timing Info
+	###
+	naive_torch_cpu_measurement = benchmark_N_iterations(
+		BENCHMARK_TRIAL_COUNT,
+		naive_torch_cpu_hbma.forward,
+		anchor_tensor,
+		target_tensor,
+	)
+
+	naive_torch_gpu_measurement = benchmark_N_iterations(
+		BENCHMARK_TRIAL_COUNT,
+		naive_torch_cpu_hbma.forward,
+		anchor_tensor,
+		target_tensor,
+	)
+
+	fused_cuda_measurement = benchmark_N_iterations(
+		BENCHMARK_TRIAL_COUNT,
+		naive_torch_cpu_hbma.forward,
+		anchor_tensor,
+		target_tensor,
+	)
+
+	###
+	### Benchmark Data handling
+	###
+	report_data = {
+		"Device Name": [torch.cuda.get_device_name(0) if torch.cuda.is_available() else 'CPU'],
+		"Image Size [B,C,H,W]": [tuple(anchor_tensor.shape)],
+		"Benchmark Trial Count": [BENCHMARK_TRIAL_COUNT],
+		"HBMA Levels": [naive_torch_cpu_hbma.levels],
+		"HBMA Block Size": [tuple(naive_torch_cpu_hbma.block_size[0])],
+		"HBMA Max Neighbor Search Distance": [naive_torch_cpu_hbma.block_max_neighbor_search_distance],
+		"Naive Torch CPU Median Latency (ms)": [1e3 * naive_torch_cpu_measurement.median],
+		"Naive Torch GPU Median Latency (ms)": [1e3 * naive_torch_gpu_measurement.median],
+		"Fused CUDA HBMA Median Latency (ms)": [1e3 * fused_cuda_measurement.median],
+
+	}
+	
+	output_csv_path = os.path.join(args.output_dir, "benchmark_results.csv")
+	output_md_path = os.path.join(args.output_dir, "benchmark_results.md")
+	report_dataframe = pandas.DataFrame(report_data)
+	report_dataframe.to_csv(output_csv_path, index=False, mode="w+")
+	report_dataframe.to_markdown(output_md_path, index=False, mode="w+")
+
+	### Save dataframe to CSV
+	print(f"Benchmark results saved to {output_csv_path} and {output_md_path}")
+
 
 
 if __name__ == "__main__":
